ff9837a700d8c25a6c012879af60ecd61215cb80
ebuki <ebuki@chemaxon.com>
2016-03-22 11:30:46 +0100

    input monomer has been changed for 'testBuildMoleculeFromSinglePolymerCHEMUnknownWithException' in BuilderMoleculeTest.

--- src/test/java/org/helm/notation2/tools/BuilderMoleculeTest.java
+++ src/test/java/org/helm/notation2/tools/BuilderMoleculeTest.java
@@ -126,7 +126,7 @@ public class BuilderMoleculeTest {
       HELM2HandledException, NotationException, ChemistryException {
 
     PolymerNotation node = new PolymerNotation("CHEM1");
-    MonomerNotationUnit mon = new MonomerNotationUnit("[L]",
+    MonomerNotationUnit mon = new MonomerNotationUnit("[Lx]",
         node.getPolymerID().getType());
     node.getPolymerElements().getListOfElements().add(mon);
     BuilderMolecule.buildMoleculefromSinglePolymer(new PolymerNotation(node.getPolymerID(), node.getPolymerElements(),
